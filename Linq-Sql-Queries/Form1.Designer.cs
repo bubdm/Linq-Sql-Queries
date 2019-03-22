bf0c6dbd24b99b3007d89fef71652794bda46cfc
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2019-03-22 11:45:06 +0300

    Linq-to-Sql

---
+++ Linq-Sql-Queries/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Linq_Sql_Queries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTextbox = new System.Windows.Forms.TextBox();
+            this.Deletebtn = new System.Windows.Forms.Button();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTextBox3 = new System.Windows.Forms.TextBox();
+            this.txtTextBox2 = new System.Windows.Forms.TextBox();
+            this.Savebtn = new System.Windows.Forms.Button();
+            this.txttextBox5 = new System.Windows.Forms.TextBox();
+            this.txttextBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTextbox
+            // 
+            this.txtTextbox.Location = new System.Drawing.Point(3, 3);
+            this.txtTextbox.Name = "txtTextbox";
+            this.txtTextbox.Size = new System.Drawing.Size(100, 20);
+            this.txtTextbox.TabIndex = 0;
+            // 
+            // Deletebtn
+            // 
+            this.Deletebtn.Location = new System.Drawing.Point(3, 40);
+            this.Deletebtn.Name = "Deletebtn";
+            this.Deletebtn.Size = new System.Drawing.Size(100, 23);
+            this.Deletebtn.TabIndex = 1;
+            this.Deletebtn.Text = "Delete";
+            this.Deletebtn.UseVisualStyleBackColor = true;
+            this.Deletebtn.Click += new System.EventHandler(this.Deletebtn_Click);
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Location = new System.Drawing.Point(109, 74);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(100, 23);
+            this.updatebtn.TabIndex = 2;
+            this.updatebtn.Text = "Update";
+            this.updatebtn.UseVisualStyleBackColor = true;
+            this.updatebtn.Click += new System.EventHandler(this.updatebtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtTextbox);
+            this.panel1.Controls.Add(this.Deletebtn);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(110, 78);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtTextBox3);
+            this.panel2.Controls.Add(this.txtTextBox2);
+            this.panel2.Controls.Add(this.updatebtn);
+            this.panel2.Location = new System.Drawing.Point(15, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(212, 100);
+            this.panel2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(5, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "id to be changed";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(4, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "value to be changed";
+            // 
+            // txtTextBox3
+            // 
+            this.txtTextBox3.Location = new System.Drawing.Point(109, 28);
+            this.txtTextBox3.Name = "txtTextBox3";
+            this.txtTextBox3.Size = new System.Drawing.Size(100, 20);
+            this.txtTextBox3.TabIndex = 4;
+            // 
+            // txtTextBox2
+            // 
+            this.txtTextBox2.Location = new System.Drawing.Point(109, 2);
+            this.txtTextBox2.Name = "txtTextBox2";
+            this.txtTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtTextBox2.TabIndex = 3;
+            // 
+            // Savebtn
+            // 
+            this.Savebtn.Location = new System.Drawing.Point(293, 55);
+            this.Savebtn.Name = "Savebtn";
+            this.Savebtn.Size = new System.Drawing.Size(100, 23);
+            this.Savebtn.TabIndex = 5;
+            this.Savebtn.Text = "Save";
+            this.Savebtn.UseVisualStyleBackColor = true;
+            this.Savebtn.Click += new System.EventHandler(this.Savebtn_Click);
+            // 
+            // txttextBox5
+            // 
+            this.txttextBox5.Location = new System.Drawing.Point(293, 29);
+            this.txttextBox5.Name = "txttextBox5";
+            this.txttextBox5.Size = new System.Drawing.Size(100, 20);
+            this.txttextBox5.TabIndex = 7;
+            // 
+            // txttextBox4
+            // 
+            this.txttextBox4.Location = new System.Drawing.Point(293, 3);
+            this.txttextBox4.Name = "txttextBox4";
+            this.txttextBox4.Size = new System.Drawing.Size(100, 20);
+            this.txttextBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(164, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Description";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(164, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "ProductDescriptionID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 254);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txttextBox4);
+            this.Controls.Add(this.txttextBox5);
+            this.Controls.Add(this.Savebtn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextbox;
+        private System.Windows.Forms.Button Deletebtn;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtTextBox3;
+        private System.Windows.Forms.TextBox txtTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Savebtn;
+        private System.Windows.Forms.TextBox txttextBox5;
+        private System.Windows.Forms.TextBox txttextBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Linq-Sql-Queries/Form1.cs
@@ -0,0 +1,54 @@
+﻿using Linq_Sql_Queries.Data_Sql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linq_Sql_Queries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        AdventureWorksLT2017Entities db = new AdventureWorksLT2017Entities();
+        private void Deletebtn_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(txtTextbox.Text);
+            var delete = db.Customers.Single(c=>c.CustomerID==value);
+            db.Customers.Remove(delete);
+            db.SaveChanges();
+            MessageBox.Show("Transaction Successful");
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(txtTextBox2.Text);
+            var customer = db.Customers.Single(c=>c.CustomerID==value);
+            customer.EmailAddress = txtTextBox3.Text;
+            db.SaveChanges();
+            MessageBox.Show("Transaction Successful");
+        }
+
+        private void Savebtn_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(txttextBox5.Text);
+            ProductDescription description = new ProductDescription()
+            {
+                 rowguid=Guid.NewGuid(),
+                  Description=txttextBox4.Text,
+                  ProductDescriptionID=value,
+                   ModifiedDate=DateTime.Now
+            };
+            db.ProductDescriptions.Add(description);
+            db.SaveChanges();
+            MessageBox.Show("Transaction Successful");
+        }
+    }
+}
